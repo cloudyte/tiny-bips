c026573b41ab95a5e60533d3836bc6ad2c522ac1
andbaz <dev@andbaz.slmail.me>
2025-12-28 23:16:19 +0200

    initial commit

---
+++ test/Cloudyte.TinyBips.Test/EmptyTest.cs
@@ -0,0 +1,13 @@
+﻿using AwesomeAssertions;
+using Xunit;
+
+namespace Cloudyte.TinyBips.Test;
+
+public sealed class EmptyTest
+{
+    [Fact]
+    public void EmptyTest_Run()
+    {
+        true.Should().BeTrue();
+    }
+}
